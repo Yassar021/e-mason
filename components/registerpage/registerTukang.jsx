eccb311db5054bebe22324c62bcecd91daeeb20c
Yassar021 <yassar.tkj114@gmail.com>
2023-05-21 14:32:24 +0700

    chore: add function register tukang

--- components/registerpage/registerTukang.jsx
+++ components/registerpage/registerTukang.jsx
@@ -1,27 +1,77 @@
-import { Box, Button, Center, Flex, Input, InputGroup, InputRightElement, Link, Stack, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Input, InputGroup, InputRightElement, Link, Stack, Text, VStack, useToast } from "@chakra-ui/react";
 import { useState } from "react";
 import LayoutAuth from "../../layout/LayoutAuth";
 
-const RegisterTukang = () => {
-    const [show, setShow] = useState(false);
-    const handleClick = () => setShow(!show);
+import { httpsCallable } from "firebase/functions";
+import functions from "../../utils/firebase/function";
 
-    return(
+const RegisterTukang = () => {
+    const [loading, setLoading] = useState(false);
+    const toast = useToast()
+    const [show, setShow] = useState(false);
+    const [field, setField] = useState({
+        nama: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        tanggalLahir: "",
+        nomorTelepon: "",
+        alamat: "",
+    })
+    const handleClick = () => setShow(!show);
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        if (field.password !== field.confirmPassword)
+            return toast({
+                title: 'Pastikan password sama!',
+                status: 'warning',
+                duration: 9000,
+                isClosable: true,
+            })
+        setLoading(true);
+        try {
+            const createUser = httpsCallable(functions, 'createUser');
+            const result = await createUser({
+                ...field,
+                type: "tukang",
+            })
+            setLoading(false);
+            // console.log(result);
+            toast({
+                title: 'Akun berhasi dibuat',
+                description: "Kami telah buat untuk anda.",
+                status: 'success',
+                duration: 9000,
+                isClosable: true,
+            })
+        } catch (error) {
+            toast({
+                title: 'Akun gagal dibuat',
+                description: "Kami gagal buat untuk anda.",
+                status: 'error',
+                duration: 9000,
+                isClosable: true,
+            })
+            setLoading(false);
+        }
+    }
+
+    return (
         <LayoutAuth pageTitle={'Register Tukang'}>
             <Box>
                 <Center>
                     <Text fontFamily={'Poppins'} fontSize='40px' fontWeight={'500'}>Registrasi Tukang</Text>
                 </Center>
 
-                <Flex direction={'column'} gap='24px'>
+                <Flex as={"form"} onSubmit={handleRegister} direction={'column'} gap='24px'>
                     <VStack spacing={'10px'} alignItems='baseline'>
                         <Text fontSize={'20px'} fontWeight='500' fontFamily={'Poppins'}>Nama</Text>
-                        <Input placeholder='masukkan nama anda' size='lg' />
-                    </VStack> 
+                        <Input onChange={(e) => setField(field => ({ ...field, nama: e.target.value }))} placeholder='masukkan nama anda' value={field.nama} size='lg' required />
+                    </VStack>
 
                     <VStack spacing={'10px'} alignItems='baseline'>
                         <Text fontSize={'20px'} fontWeight='500' fontFamily={'Poppins'}>Email Address</Text>
-                        <Input placeholder='masukkan email anda' size='lg' />
+                        <Input type="email" onChange={(e) => setField(field => ({ ...field, email: e.target.value }))} placeholder='masukkan email anda' value={field.email} size='lg' required />
                     </VStack>
 
                     <VStack spacing={'10px'} alignItems='baseline'>
@@ -31,6 +81,10 @@ const RegisterTukang = () => {
                                 pr='4.5rem'
                                 type={show ? 'text' : 'password'}
                                 placeholder='masukkan password anda'
+                                value={field.password}
+                                min={6}
+                                required
+                                onChange={(e) => setField(field => ({ ...field, password: e.target.value }))}
                             />
                             <InputRightElement width='4.5rem'>
                                 <Button h='1.75rem' size='sm' onClick={handleClick}>
@@ -47,6 +101,10 @@ const RegisterTukang = () => {
                                 pr='4.5rem'
                                 type={show ? 'text' : 'password'}
                                 placeholder='masukkan ulang password anda'
+                                value={field.confirmPassword}
+                                min={6}
+                                required
+                                onChange={(e) => setField(field => ({ ...field, confirmPassword: e.target.value }))}
                             />
                             <InputRightElement width='4.5rem'>
                                 <Button h='1.75rem' size='sm' onClick={handleClick}>
@@ -57,41 +115,41 @@ const RegisterTukang = () => {
                     </VStack>
 
                     <VStack spacing={'10px'} alignItems='baseline'>
-                        <Text fontSize={'20px'} fontWeight='500' fontFamily={'Poppins'}>Usia</Text>
-                        <Input placeholder='masukkan usia anda' size='lg' />
+                        <Text fontSize={'20px'} fontWeight='500' fontFamily={'Poppins'}>Tanggal Lahir</Text>
+                        <Input type="date" placeholder='masukkan tanggal lahir anda' value={field.tanggalLahir} size='lg' required onChange={(e) => setField(field => ({ ...field, tanggalLahir: e.target.value }))} />
                     </VStack>
 
                     <VStack spacing={'10px'} alignItems='baseline'>
                         <Text fontSize={'20px'} fontWeight='500' fontFamily={'Poppins'}>Nomor Telepon</Text>
-                        <Input placeholder='masukkan telepon anda' size='lg' />
+                        <Input type="number" required placeholder='masukkan telepon anda' size='lg' value={field.nomorTelepon} onChange={(e) => setField(field => ({ ...field, nomorTelepon: e.target.value }))} />
                     </VStack>
 
                     <VStack spacing={'10px'} alignItems='baseline'>
                         <Text fontSize={'20px'} fontWeight='500' fontFamily={'Poppins'}>Alamat</Text>
-                        <Input placeholder='masukkan alamat anda' size='lg' />
+                        <Input required placeholder='masukkan alamat anda' size='lg' value={field.alamat} onChange={(e) => setField(field => ({ ...field, alamat: e.target.value }))} />
                     </VStack>
 
-                    <Link href="/dashboardTukangPage">
-                        <Button
-                            mt='46px'
-                            size='md'
-                            height='60px'
-                            width={'100%'}
-                            color={'#fff'}
-                            bgColor='#3E38F5'
-                            borderRadius={'6px'}
-                            _hover={{ bg: '#3E38F5' }}
-                            _active={{
+                    <Button
+                        isLoading={loading}
+                        mt='46px'
+                        size='md'
+                        height='60px'
+                        width={'100%'}
+                        color={'#fff'}
+                        bgColor='#3E38F5'
+                        borderRadius={'6px'}
+                        _hover={{ bg: '#3E38F5' }}
+                        _active={{
                             bg: '#3E38F5',
                             transform: 'scale(0.98)',
-                            }}
-                            >
-                            Login
-                        </Button>
-                    </Link>
+                        }}
+                        type="submit"
+                    >
+                        Daftar
+                    </Button>
 
                     <Center>
-                        <Stack direction={{base:'column',md:'row'}} spacing={'10px'} mt='24px'>
+                        <Stack direction={{ base: 'column', md: 'row' }} spacing={'10px'} mt='24px'>
                             <Text fontSize={'18px'} fontWeight='500' fontFamily={'Poppins'}>Sudah Punya Akun ?</Text>
                             <Link href='/' >
                                 <Text fontSize={'18px'} fontWeight='500' fontFamily={'Poppins'} color='#3E38F5'>Login</Text>
---
+++ utils/firebase/function.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFunctions } from 'firebase/functions';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1NhakolQdQSyGyT1EfHOjtU0jco-4Ma4",
+  authDomain: "emason-c2ba1.firebaseapp.com",
+  projectId: "emason-c2ba1",
+  storageBucket: "emason-c2ba1.appspot.com",
+  messagingSenderId: "949703525469",
+  appId: "1:949703525469:web:c6c2a2a8010ce483abb9b4"
+};
+
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app, "asia-southeast1");
+
+export default functions;
