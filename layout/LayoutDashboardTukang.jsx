9139c846e662b7fcecfeacaa2a1e258e34edea67
Yassar021 <yassar.tkj114@gmail.com>
2023-05-31 02:27:07 +0800

    fix: typo ATTR SVG

--- layout/LayoutDashboardTukang.jsx
+++ layout/LayoutDashboardTukang.jsx
@@ -33,7 +33,7 @@ const LayoutDashboardTukang = ({ pageTitle, children, bgColor, color }) => {
                                         <Stack direction={'row'} spacing='12px'>
                                             <Stack mt='4px'>
                                                 <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M1 3C1 3 2.5 1 6 1C9.5 1 11 3 11 3V17C11 17 9.5 16 6 16C2.5 16 1 17 1 17V3ZM11 3C11 3 12.5 1 16 1C19.5 1 21 3 21 3V17C21 17 19.5 16 16 16C12.5 16 11 17 11 17V3Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <path d="M1 3C1 3 2.5 1 6 1C9.5 1 11 3 11 3V17C11 17 9.5 16 6 16C2.5 16 1 17 1 17V3ZM11 3C11 3 12.5 1 16 1C19.5 1 21 3 21 3V17C21 17 19.5 16 16 16C12.5 16 11 17 11 17V3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                                 </svg>
                                             </Stack>
                                             <Text color={color} fontSize={'20px'} fontWeight='600'>Negosiasi</Text>
@@ -48,8 +48,8 @@ const LayoutDashboardTukang = ({ pageTitle, children, bgColor, color }) => {
                                         <Stack direction={'row'} spacing='12px'>
                                             <Stack mt='4px'>
                                                 <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M11 1C5.477 1 1 5.477 1 11C1 16.523 5.477 21 11 21C16.523 21 21 16.523 21 11C21 5.477 16.523 1 11 1Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                    <path d="M3.271 17.346C3.271 17.346 5.5 14.5 11 14.5C16.5 14.5 18.73 17.346 18.73 17.346M11 11C11.7956 11 12.5587 10.6839 13.1213 10.1213C13.6839 9.55871 14 8.79565 14 8C14 7.20435 13.6839 6.44129 13.1213 5.87868C12.5587 5.31607 11.7956 5 11 5C10.2043 5 9.44128 5.31607 8.87868 5.87868C8.31607 6.44129 8 7.20435 8 8C8 8.79565 8.31607 9.55871 8.87868 10.1213C9.44128 10.6839 10.2043 11 11 11V11Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <path d="M11 1C5.477 1 1 5.477 1 11C1 16.523 5.477 21 11 21C16.523 21 21 16.523 21 11C21 5.477 16.523 1 11 1Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                    <path d="M3.271 17.346C3.271 17.346 5.5 14.5 11 14.5C16.5 14.5 18.73 17.346 18.73 17.346M11 11C11.7956 11 12.5587 10.6839 13.1213 10.1213C13.6839 9.55871 14 8.79565 14 8C14 7.20435 13.6839 6.44129 13.1213 5.87868C12.5587 5.31607 11.7956 5 11 5C10.2043 5 9.44128 5.31607 8.87868 5.87868C8.31607 6.44129 8 7.20435 8 8C8 8.79565 8.31607 9.55871 8.87868 10.1213C9.44128 10.6839 10.2043 11 11 11V11Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                                 </svg>
                                             </Stack>
                                             <Text color={color} fontSize={'20px'} fontWeight='600'>Ubah Profile</Text>
@@ -64,10 +64,10 @@ const LayoutDashboardTukang = ({ pageTitle, children, bgColor, color }) => {
                                         <Stack direction={'row'} spacing='12px'>
                                             <Stack mt='4px'>
                                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <rect x="5" y="4" width="14" height="17" rx="2" stroke="#000000" stroke-width="2" />
-                                                    <path d="M9 9H15" stroke="#000000" stroke-width="2" stroke-linecap="round" />
-                                                    <path d="M9 13H15" stroke="#000000" stroke-width="2" stroke-linecap="round" />
-                                                    <path d="M9 17H13" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+                                                    <rect x="5" y="4" width="14" height="17" rx="2" stroke="#000000" strokeWidth="2" />
+                                                    <path d="M9 9H15" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+                                                    <path d="M9 13H15" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+                                                    <path d="M9 17H13" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
                                                 </svg>
                                             </Stack>
                                             <Text color={color} fontSize={'20px'} fontWeight='600'>Portofolio</Text>
@@ -82,9 +82,9 @@ const LayoutDashboardTukang = ({ pageTitle, children, bgColor, color }) => {
                         <Hide above='lg'>
                             <Button
                                 leftIcon={<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M5 7H19" stroke="#CCD2E3" stroke-width="2" stroke-linecap="round" />
-                                    <path d="M5 12H19" stroke="#CCD2E3" stroke-width="2" stroke-linecap="round" />
-                                    <path d="M5 17H19" stroke="#CCD2E3" stroke-width="2" stroke-linecap="round" />
+                                    <path d="M5 7H19" stroke="#CCD2E3" strokeWidth="2" strokeLinecap="round" />
+                                    <path d="M5 12H19" stroke="#CCD2E3" strokeWidth="2" strokeLinecap="round" />
+                                    <path d="M5 17H19" stroke="#CCD2E3" strokeWidth="2" strokeLinecap="round" />
                                 </svg>
                                 }
                                 mb='20px'
@@ -119,7 +119,7 @@ const LayoutDashboardTukang = ({ pageTitle, children, bgColor, color }) => {
                                                 <Stack direction={'row'} spacing='12px'>
                                                     <Stack mt='4px'>
                                                         <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M1 3C1 3 2.5 1 6 1C9.5 1 11 3 11 3V17C11 17 9.5 16 6 16C2.5 16 1 17 1 17V3ZM11 3C11 3 12.5 1 16 1C19.5 1 21 3 21 3V17C21 17 19.5 16 16 16C12.5 16 11 17 11 17V3Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M1 3C1 3 2.5 1 6 1C9.5 1 11 3 11 3V17C11 17 9.5 16 6 16C2.5 16 1 17 1 17V3ZM11 3C11 3 12.5 1 16 1C19.5 1 21 3 21 3V17C21 17 19.5 16 16 16C12.5 16 11 17 11 17V3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                                         </svg>
                                                     </Stack>
                                                     <Text color={color} fontSize={'20px'} fontWeight='600'>Negosiasi</Text>
@@ -134,8 +134,8 @@ const LayoutDashboardTukang = ({ pageTitle, children, bgColor, color }) => {
                                                 <Stack direction={'row'} spacing='12px'>
                                                     <Stack mt='4px'>
                                                         <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M11 1C5.477 1 1 5.477 1 11C1 16.523 5.477 21 11 21C16.523 21 21 16.523 21 11C21 5.477 16.523 1 11 1Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                            <path d="M3.271 17.346C3.271 17.346 5.5 14.5 11 14.5C16.5 14.5 18.73 17.346 18.73 17.346M11 11C11.7956 11 12.5587 10.6839 13.1213 10.1213C13.6839 9.55871 14 8.79565 14 8C14 7.20435 13.6839 6.44129 13.1213 5.87868C12.5587 5.31607 11.7956 5 11 5C10.2043 5 9.44128 5.31607 8.87868 5.87868C8.31607 6.44129 8 7.20435 8 8C8 8.79565 8.31607 9.55871 8.87868 10.1213C9.44128 10.6839 10.2043 11 11 11V11Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M11 1C5.477 1 1 5.477 1 11C1 16.523 5.477 21 11 21C16.523 21 21 16.523 21 11C21 5.477 16.523 1 11 1Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                                            <path d="M3.271 17.346C3.271 17.346 5.5 14.5 11 14.5C16.5 14.5 18.73 17.346 18.73 17.346M11 11C11.7956 11 12.5587 10.6839 13.1213 10.1213C13.6839 9.55871 14 8.79565 14 8C14 7.20435 13.6839 6.44129 13.1213 5.87868C12.5587 5.31607 11.7956 5 11 5C10.2043 5 9.44128 5.31607 8.87868 5.87868C8.31607 6.44129 8 7.20435 8 8C8 8.79565 8.31607 9.55871 8.87868 10.1213C9.44128 10.6839 10.2043 11 11 11V11Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                                         </svg>
                                                     </Stack>
                                                     <Text color={color} fontSize={'20px'} fontWeight='600'>Ubah Profile</Text>
@@ -150,10 +150,10 @@ const LayoutDashboardTukang = ({ pageTitle, children, bgColor, color }) => {
                                                 <Stack direction={'row'} spacing='12px'>
                                                     <Stack mt='4px'>
                                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <rect x="5" y="4" width="14" height="17" rx="2" stroke="#000000" stroke-width="2" />
-                                                            <path d="M9 9H15" stroke="#000000" stroke-width="2" stroke-linecap="round" />
-                                                            <path d="M9 13H15" stroke="#000000" stroke-width="2" stroke-linecap="round" />
-                                                            <path d="M9 17H13" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+                                                            <rect x="5" y="4" width="14" height="17" rx="2" stroke="#000000" strokeWidth="2" />
+                                                            <path d="M9 9H15" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+                                                            <path d="M9 13H15" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
+                                                            <path d="M9 17H13" stroke="#000000" strokeWidth="2" strokeLinecap="round" />
                                                         </svg>
                                                     </Stack>
                                                     <Text color={color} fontSize={'20px'} fontWeight='600'>Portofolio</Text>
